e68240b485386396ae2cbe46dcdbcf676d1f4bfb
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-02-07 00:46:41 -0500

    Created landing page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Typography, Box, TextField, Paper } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header class="App-header">Hello! I'm Gabrielle Noelle Donald 🌩️</header>
+    </>
   );
 }
 
