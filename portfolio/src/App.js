7d613d5ac3193c9445d657a67f7c30a1ae954b9d
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-02-08 14:19:06 -0500

    added additional styling and description

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
 import "./App.css";
-import { Typography, Box, TextField, Paper } from "@mui/material";
+import { Typography, Box } from "@mui/material";
+import "@fontsource/roboto";
 
 function App() {
   return (
     <>
-      <header class="App-header">Hello! I'm Gabrielle Noelle Donald 🌩️</header>
+      <div>
+        <header class="App-header">
+          <Typography variant="h3">
+            Hello! I'm Gabrielle Noelle Donald 🌩️
+          </Typography>
+        </header>
+      </div>
+      <div class="desc">
+        <Box
+          display="flex"
+          width={500}
+          height={200}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography variant="subtitle2">
+            ⛈️ Software engineer, full-stack developer
+            <br />
+            <br />
+            ☁️ Passionate, dedicated, tenacious
+            <br />
+            <br />⚡ Certificate in Full-Stack Web Development from UCF
+          </Typography>
+        </Box>
+      </div>
     </>
   );
 }
